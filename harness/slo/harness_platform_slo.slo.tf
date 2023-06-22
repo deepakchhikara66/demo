c2f1552b1889c2a0f9f552a1389b00ef27383f54
Deepak <deepakchhikara66@gmail.com>
2023-06-22 17:06:47 +0530

    Update harness_platform_slo.slo.tf

--- harness/slo/harness_platform_slo.slo.tf
+++ harness/slo/harness_platform_slo.slo.tf
@@ -12,7 +12,7 @@ resource "harness_platform_slo" "slo" {
     user_journey_refs = ["one", "two"]
     slo_target {
       type                  = "Calender"
-      slo_target_percentage = 93
+      slo_target_percentage = 92
       spec                  = jsonencode({
         type = "Monthly"
         spec = {
