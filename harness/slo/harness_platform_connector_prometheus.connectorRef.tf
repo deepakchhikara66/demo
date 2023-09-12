c6a167318436b7af12d13a752b098056565efeae
Varsha Lalwani <varsha.lalwani@harness.io>
2023-09-12 19:55:44 +0530

    Update harness_platform_connector_prometheus.connectorRef.tf

--- harness/slo/harness_platform_connector_prometheus.connectorRef.tf
+++ harness/slo/harness_platform_connector_prometheus.connectorRef.tf
@@ -8,6 +8,6 @@ resource "harness_platform_connector_prometheus" "connectorRef" {
   tags        = ["foo:bar"]
   org_id = harness_platform_organization.terraform_org.id
   project_id = harness_platform_project.terraform_project.id
-  delegate_selectors = ["stress-chi-play-med-ng"]
+  delegate_selectors = ["chi-play-prod2-small"]
   url = "http://10.4.2.99:80/health-source/prometheus/"
 }
