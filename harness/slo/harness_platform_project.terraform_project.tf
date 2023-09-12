5f6f5e979bfec3ed1bdc67e8b1a642eacad011d2
Varsha Lalwani <varsha.lalwani@harness.io>
2023-09-12 22:09:03 +0530

    Update harness_platform_project.terraform_project.tf

--- harness/slo/harness_platform_project.terraform_project.tf
+++ harness/slo/harness_platform_project.terraform_project.tf
@@ -1,9 +1,6 @@
 resource "harness_platform_project" "terraform_project" {
-	depends_on = [
-		harness_platform_organization.terraform_org
-	]
 	identifier = "terraform_project"
 	name = "terraform_project"
-	org_id = harness_platform_organization.terraform_org.id
+	org_id = "cvng"
 	color = "#472848"
 }
