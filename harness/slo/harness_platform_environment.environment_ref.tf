97ed25b00817eb5c29fb13c9cf6513fa8a2ee558
Varsha Lalwani <varsha.lalwani@harness.io>
2023-09-13 00:45:24 +0530

    Update harness_platform_environment.environment_ref.tf

--- harness/slo/harness_platform_environment.environment_ref.tf
+++ harness/slo/harness_platform_environment.environment_ref.tf
@@ -1,7 +1,4 @@
 resource "harness_platform_environment" "environment_ref" {
-	depends_on = [
-		harness_platform_project.terraform_project
-	]
 	identifier = "environment_ref"
 	name = "environment_ref"
 	org_id = "cvng"
