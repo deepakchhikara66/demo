529d2ddfd96b0ac4d3b57970fa017281cb9a79a4
Varsha Lalwani <varsha.lalwani@harness.io>
2023-09-12 18:47:44 +0530

    Update harness_provider.tf

--- harness/slo/harness_provider.tf
+++ harness/slo/harness_provider.tf
@@ -8,5 +8,5 @@ terraform {
   provider "harness" {
     endpoint         = "https://qa.harness.io"
     account_id       = "-k53qRQAQ1O7DBLb9ACnjQ"
-    platform_api_key = "pat.-k53qRQAQ1O7DBLb9ACnjQ.63eb6a8f9aae3a716967a51b.kMS1Ro6qzbZ5Jjem1ppn"
+    platform_api_key = "pat.-k53qRQAQ1O7DBLb9ACnjQ.650064e102854a1a998916de.ZpRHBMqy9UkgUhOneGeG"
   }
