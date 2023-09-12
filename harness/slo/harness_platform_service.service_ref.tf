075bac125283a42e60829de36c4f52b42b0ea4dc
Varsha Lalwani <varsha.lalwani@harness.io>
2023-09-13 00:46:32 +0530

    Update harness_platform_service.service_ref.tf

--- harness/slo/harness_platform_service.service_ref.tf
+++ harness/slo/harness_platform_service.service_ref.tf
@@ -1,7 +1,4 @@
 resource "harness_platform_service" "service_ref" {
-  depends_on = [
-    harness_platform_project.terraform_project
-  ]
   identifier = "service_ref"
   name = "service_ref"
   org_id = "cvng"
